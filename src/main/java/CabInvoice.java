baf6de2b8951f5e843a5f64a379274d75c42a35c
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-25 08:09:10 +0530

    Added : Welcome to Cab Invoice Summary

---
+++ src/main/java/CabInvoice.java
@@ -0,0 +1,7 @@
+public class CabInvoice {
+
+    public double invoiceGenerator(double distance, int time) {
+        double result = distance * 10 + time * 1;
+        return result;
+    }
+}
---
+++ src/test/java/CabInvoiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2;
+        int time = 2;
+        CabInvoice cabInvoice = new CabInvoice();
+        double invoice = cabInvoice.invoiceGenerator(distance, time);
+        Assert.assertEquals(22, invoice, 0.0);
+
+    }
+}
