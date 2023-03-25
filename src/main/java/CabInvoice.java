fc89298863785ac30d1636926c059e3680986233
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-25 08:55:13 +0530

    Added : Code to Calculate total Fare for Multiple Ride

--- src/main/java/CabInvoice.java
+++ src/main/java/CabInvoice.java
@@ -2,6 +2,17 @@ public class CabInvoice {
 
     public double invoiceGenerator(double distance, int time) {
         double result = distance * 10 + time * 1;
+        if (result<5){
+            return 5;
+        }
         return result;
+    }
+
+    public double invoiceGenerator(Ride[] rides) {
+        double totalFare=0;
+        for (Ride ride: rides) {
+           totalFare+= this.invoiceGenerator(ride.distance,ride.time);
+        }
+        return totalFare;
     }
 }
--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -11,4 +11,21 @@ public class CabInvoiceTest {
         double invoice = cabInvoice.invoiceGenerator(distance, time);
         Assert.assertEquals(22, invoice, 0.0);
     }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 2;
+        CabInvoice cabInvoice = new CabInvoice();
+        double invoice = cabInvoice.invoiceGenerator(distance, time);
+        Assert.assertEquals(5, invoice, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ForMultipleRide_ShouldReturnTotalFare() {
+        CabInvoice cabInvoice = new CabInvoice();
+        Ride[] rides = {new Ride(2,2),new Ride(5,1)};
+        double invoice = cabInvoice.invoiceGenerator(rides);
+        Assert.assertEquals(73, invoice, 0.0);
+    }
 }
